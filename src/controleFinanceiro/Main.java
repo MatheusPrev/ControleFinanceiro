43742c136491e6c48db7bd885840f32fa31d6801
Matheus Previatti <m.g.previatti@uol.com.br>
2022-03-25 19:44:30 -0300

    finalizacao de trabalho do dia 1

--- src/controleFinanceiro/Main.java
+++ src/controleFinanceiro/Main.java
@@ -12,17 +12,18 @@ public class Main {
 	static Scanner entrada = new Scanner(System.in);
 	static JFrame frame = new JFrame();
 	static Banco Itau = new Banco("Itau",15495168,13209);
-	static Banco Nubanc = new Banco("Nubanc",9831887,7013524);
-	static Banco[] bancos = {Itau,Nubanc};
+	static Banco Nubank = new Banco("Nubank",9831887,7013524);
+	static Banco[] bancos = {Itau,Nubank};
 
 	public static void main(String[] args) {
 		
 		Bens ItauCC = new Bens(Itau,"Conta corrente",225.32);
 		Bens ItauCredito = new Bens(Itau,"Credito",-25.45);
 		Bens ItauPrevidencia = new Bens(Itau,"Previdencia",5231.17);
-		Bens NubancCC = new Bens(Nubanc,"Conta corrente",107.05);
-		Bens NubancCredito = new Bens(Nubanc,"Credito",-51.14);
-		Bens[] patrimonio = {ItauCC,ItauCredito,ItauPrevidencia,NubancCC,NubancCredito};
+		Bens NubancCC = new Bens(Nubank,"Conta corrente",107.05);
+		Bens NubancCredito = new Bens(Nubank,"Credito",-51.14);
+		Bens ItauCDB = new Bens(Itau,"CDB",7127.64);
+		Bens[] patrimonio = {ItauCC,ItauCredito,ItauPrevidencia,NubancCC,ItauCDB,NubancCredito};
 		
 		int opc = menu(patrimonio);
 		System.out.println(opc);
@@ -60,16 +61,9 @@ public class Main {
 		}
 		
 		JLabel label1 = new JLabel();
-		label1.setText("Total: "+ valorTotal);
-		//ImageIcon image = new ImageIcon("C:\\Users\\mathe\\eclipse-workspace\\ControleFinanceiro\\midia\\icone1.png");
-		//label.setIcon(image);
-		//label.setHorizontalAlignment(JLabel.LEFT);
-		//label.setVerticalAlignment(JLabel.TOP);
+		label1.setText("Total: R$"+ valorTotal);
 		label1.setForeground(Color.green);
 		label1.setFont(new Font("Arial",Font.BOLD,30));
-		label1.setBounds(7,7,210,30);
-		//label.setBackground(Color.white);
-		//label.setOpaque(true);
 		
 		JLabel[] tituloBanco = new JLabel[bancos.length];
 		JLabel[] valoresMenu = new JLabel[patrimonio.length];
@@ -77,25 +71,23 @@ public class Main {
 		for(int i=0;i<bancos.length;i++) {
 			tituloBanco[i] = new JLabel();
 			tituloBanco[i].setText(bancos[i].banco);
-			tituloBanco[i].setFont(new Font("Arial",Font.BOLD,18));
+			tituloBanco[i].setFont(new Font("Arial",Font.BOLD,22));
+			tituloBanco[i].setForeground(Color.white);
 		}
 		for(int i=0;i<patrimonio.length;i++) {
 		    valoresMenu[i] = new JLabel();
 		    tipoMenu[i] = new JLabel();
 		    tipoMenu[i].setText(patrimonio[i].tipoConta);
 			valoresMenu[i].setText("R$ " + patrimonio[i].valor);
-			//tipoMenu[i].setForeground(Color.white);
-			//valoresMenu[i].setForeground(Color.white);
 			tipoMenu[i].setFont(new Font("Arial",Font.BOLD,18));
 			valoresMenu[i].setFont(new Font("Arial",Font.BOLD,18));
-			//tipoMenu[i].setBounds(7,7,200,20);
-			//valoresMenu[i].setBounds(7,7,200,20);
-			//valoresMenu[i].setHorizontalAlignment(JLabel.LEFT);
+			tipoMenu[i].setForeground(Color.white);
+			valoresMenu[i].setForeground(Color.white);
 		}
 		
 		JPanel total = new JPanel();
-		total.setBounds(5,25,250,40);
-		//total.setBackground(Color.white);
+		total.setBounds(5,25,300,40);
+		total.setBackground(null);
 		total.add(label1);
 		
 		JPanel barra = new JPanel();
@@ -113,14 +105,16 @@ public class Main {
 			vertical += 30;
 			topico[i].setBackground(bancos[i].secundaria);
 			topico[i].add(tituloBanco[i]);
+			int k = 0;
 			for(int j=0;j<patrimonio.length;j++) {
 				if(patrimonio[j].banco == bancos[i]) {
+					k++;
 					coluna1[j] = new JPanel();
 					coluna2[j] = new JPanel();
 					coluna1[j].setBounds(5,vertical,200,25);
 					coluna2[j].setBounds(205,vertical,100,25);
 					vertical += 25;
-					if(j%2==0) {
+					if(k%2==0) {
 						coluna1[j].setBackground(bancos[i].primaria);
 						coluna2[j].setBackground(bancos[i].primaria);
 					}else {
@@ -145,6 +139,7 @@ public class Main {
 			myFrame.add(coluna1[i]);
 			myFrame.add(coluna2[i]);
 		}
+		myFrame.setVisible(true);
 		
 		return 0;
 	}
--- src/controleFinanceiro/MyFrame.java
+++ src/controleFinanceiro/MyFrame.java
@@ -15,7 +15,7 @@ public class MyFrame extends JFrame{
 	MyFrame(){
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setSize(500,500);
-		this.setVisible(true);
+		//this.setVisible(true);
 		ImageIcon image = new ImageIcon("C:\\Users\\mathe\\eclipse-workspace\\ControleFinanceiro\\midia\\logo.png");
 		this.setIconImage(image.getImage());
 		this.getContentPane().setBackground(new Color(0x4A4A4A));
